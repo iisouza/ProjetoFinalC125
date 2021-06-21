94d2af817a2e3aed90074efd8715e307248d2758
Igor Gonçalves <isouza@gea.inatel.br>
2021-06-21 18:56:12 -0300

    Atualizando a main

--- src/main/java/Bibliotecas/ProjetoFinal/Principal/Main.java
+++ src/main/java/Bibliotecas/ProjetoFinal/Principal/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		Path arquivo = Paths.get("ListaAtividades.txt");
 		TxtUtils.AtividadeDiaSemana(arquivo);
-		System.out.println("");
+		System.out.println(" ");
 		
 		TxtUtils.TotalPorEsporte(arquivo);
 		Interface.PlotaGrafico(arquivo);
